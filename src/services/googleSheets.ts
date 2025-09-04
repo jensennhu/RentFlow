fbe6cce1d7e44c499a95ec6225a626e5cf9cd63d
Jensen Hu <40771617+jensennhu@users.noreply.github.com>
2025-09-04 12:37:58 -0700

    Updated googleSheets.ts

--- src/services/googleSheets.ts
+++ src/services/googleSheets.ts
@@ -2,7 +2,7 @@ import { GoogleSheetsConfig, Property, Tenant, Payment } from '../types';
 import { googleAuthService } from './googleAuth';
 
 class GoogleSheetsService {
-  private readonly API_BASE = 'http://localhost:3001';
+  private readonly API_BASE = 'http://localhost:5173';
 
   getConfig(): GoogleSheetsConfig | null {
     return googleAuthService.getConfig();
